583845300038291268e0b40149db6270929d3bef
Leon a lenda <leon.kacki@aluno.educa.go.gov.br>
2026-02-19 14:03:26 +0000

    Adiciona estrutura básica do jogo e lógica de movimentação do personagem

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+const canvas = document.getElementById("jogoCanvas")
+const ctx = canvas.getContext("2d")
 
+//config. personagens
+let player = {
+    x: 360,
+    y: 0,
+    tamanho: 40,
+    velocidade: 5,
+    cor: "blue"
+};
+
+//monitor de teclas
+let teclas = {};
+window.addEventListener("keydown", (e) => teclas [e.key] = true);
+window.addEventListener("keyup", (e) => teclas [e.key] = false);
+
+function atualizar(){
+ //monvimentação no eixo X
+if(teclas["ArrowLeft"]) player.x -= player.velocidade;
+if(teclas["ArrowRight"]) player.x += player.velocidade;
+//monvimentação no eixo Y
+if(teclas["ArrowUp"]) player.y -= player.velocidade;
+if(teclas["ArrowDown"]) player.y += player.velocidade;
+}
+
+//Limpar a tela e criar o personagem
+function desenhar(){
+ctx.clearRect(0,0, canvas.width, canvas.height);  
+ctx.fillStyle = player.cor;
+ctx.fillRect (player.x,player.y, player.tamanho, player.tamanho);
+}
+
+function loop(){
+    atualizar();
+    desenhar();
+    requestAnimationFrame(loop);
+}
+loop();
